b8decb2b2ec9c108f225e15e1843dc33d7bfb0d2
Alper ÇOLAK <alpercolak@trusturk.com>
2023-10-14 20:07:35 +0300

    css root oluşturuldu, detaylandırılacak.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './view/Home';
-import About from './view/About';
-import NotFound from './view/NotFound';
-import LoginPage from './view/Login';
+import Home from './views/Home';
+import About from './views/About';
+import NotFound from './views/NotFound';
+import LoginPage from './views/Login';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <Router>
-      <div>
-        <header>
-          {/* TODO: Header eklenmeli */}
-        </header>
+      <header />
 
-        <body>
+      <Container>
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -23,12 +22,10 @@ function App() {
             {/* Eğer tanımlanmamış bir rota girilirse */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </body>
+        </main>
+      </Container>
 
-        <footer>
-          {/* TODO: Footer eklenmeli */}
-        </footer>
-      </div>
+      <footer />
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './css/index.css';
+import './css/App.css';
 import './css/site.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
