446f4c9a14a2acd793b5dace649a465d9d1c77ad
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2023-10-14 15:27:04 +0300

    Bootstarp Güncellemesi

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './site.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import LoginPage from './Pages/LoginPage';
 
