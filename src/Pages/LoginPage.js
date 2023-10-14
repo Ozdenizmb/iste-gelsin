89a2099f6be909e06132f525870958d3c6fb74be
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2023-10-14 15:01:36 +0300

    Bazı Kütüphaneler Eklendi

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Input from '../Components/Input';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const LoginPage = () => {
     return (
         <div className="container">
             <h1 className="text-center">Login Page</h1>
             <Input label={"Username"} type={"text"} name={"username"}/>
-            <Input label={"Username"} type={"password"} name={"username"}/>
+            <Input label={"Password"} type={"password"} name={"username"}/>
 
             <div className="text-center">
                     <button className="btn btn-primary">
+                    <FontAwesomeIcon icon="fa-solid fa-right-to-bracket" />
                         Login
                     </button>
                 </div>
