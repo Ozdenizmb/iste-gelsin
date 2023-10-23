58b832df65fa282861da1634279d6da45e78c23b
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2023-10-23 23:32:23 +0300

    signUp

---
+++ src/views/SignUp/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Input from '../../Components/Input';
+
+const SignUp = () => {
+    return (
+        <div className="container">
+
+            <h1 className="text-center">{t('Sign Up')}</h1>
+
+            <Input />
+
+        </div>
+    );
+};
+
+export default SignUp;
