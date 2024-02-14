84c124b3a7bd6aac17638d780fc20a19f0607852
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2024-02-14 22:44:08 +0300

    login sayfası tasarlandı

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { MDBContainer, MDBTabs, MDBTabsItem, MDBTabsLink } from 'mdb-react-ui-kit';
 import Input from '../../Components/Input';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faApple  } from '@fortawesome/free-brands-svg-icons';
+
 
 const Login = () => {
     const [justifyActive, setJustifyActive] = useState('tab1');
+    const [rememberMe, setRememberMe] = useState(false);
 
     const handleJustifyClick = (value) => {
         if (value === justifyActive) {
@@ -13,8 +18,12 @@ const Login = () => {
         setJustifyActive(value);
     };
 
+    const onRememberMeChange = (value) => {
+        setRememberMe(!rememberMe);
+    }
+
     return (
-        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50 card p-4 shadow">
             <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
                 <MDBTabsItem>
                     <MDBTabsLink onClick={() => handleJustifyClick('tab1')} active={justifyActive === 'tab1'}>
@@ -30,17 +39,64 @@ const Login = () => {
 
             {justifyActive === 'tab1' && (
                 <div>
-                    <p>Giriş Yap:</p>
+                    <h5 className="mb-4">Çalışanlar için Giriş Yap:</h5>
                     <Input label={"Email"} type={"text"} name={"Email"} />
-                    <Input label={"Şifre"} type={"text"} name={"Password"} />
+                    <Input label={"Şifre"} type={"password"} name={"Password"} />
+                    <div className="d-flex justify-content-between mb-4">
+                        <label>
+                            <input className="form-check-input me-2" type={"checkbox"} name={"RememberMe"} checked={rememberMe} onChange={onRememberMeChange}/> Beni Hatırla
+                        </label>
+                        <Link className="" to="/contact">
+                            Şifremi Unuttum
+                        </Link>
+                    </div>
+                    <div class="d-grid gap-2 col-8 mx-auto">
+                        <button class="btn btn-primary" type="button">Giriş Yap</button>
+                    </div>
+                    <div className="mt-3 d-flex">
+                        {/* Google Giriş Butonu */}
+                        <button className="btn btn-light border me-2 flex-grow-1" type="button">
+                            <FontAwesomeIcon icon={faGoogle}  className="me-2"/>
+                            Google ile Giriş Yap
+                        </button>
+                        {/* Apple Giriş Butonu */}
+                        <button className="btn btn-dark flex-grow-1" type="button">
+                            <FontAwesomeIcon icon={faApple}  className="me-2" />
+                            Apple ile Giriş Yap
+                        </button>
+                    </div>
                 </div>
             )}
 
             {justifyActive === 'tab2' && (
                 <div>
-                    <p>Giriş Yap:</p>
-                    {/* Your content for tab2 */}
+                <h5 className="mb-4">İş Verenler için Giriş Yap:</h5>
+                <Input label={"Email"} type={"text"} name={"Email"} />
+                <Input label={"Şifre"} type={"password"} name={"Password"} />
+                <div className="d-flex justify-content-between mb-4">
+                    <label>
+                        <input className="form-check-input me-2" type={"checkbox"} name={"RememberMe"} checked={rememberMe} onChange={onRememberMeChange}/> Beni Hatırla
+                    </label>
+                    <Link className="" to="/contact">
+                        Şifremi Unuttum
+                    </Link>
                 </div>
+                <div class="d-grid gap-2 col-8 mx-auto">
+                    <button class="btn btn-primary" type="button">Giriş Yap</button>
+                </div>
+                <div className="mt-3 d-flex">
+                    {/* Google Giriş Butonu */}
+                    <button className="btn btn-light border me-2 flex-grow-1" type="button">
+                        <FontAwesomeIcon icon={faGoogle}  className="me-2"/>
+                        Google ile Giriş Yap
+                    </button>
+                    {/* Apple Giriş Butonu */}
+                    <button className="btn btn-dark flex-grow-1" type="button">
+                        <FontAwesomeIcon icon={faApple}  className="me-2" />
+                        Apple ile Giriş Yap
+                    </button>
+                </div>
+            </div>
             )}
         </MDBContainer>
     );
