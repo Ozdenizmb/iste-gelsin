0e0a830358cbf5a73c4c7a00d0a75721d2992bb3
Anldmrlpp <147911313+Anldmrlpp@users.noreply.github.com>
2024-02-15 21:26:35 +0300

    sign up guncel

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,105 +1,172 @@
+
+
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { MDBContainer, MDBTabs, MDBTabsItem, MDBTabsLink } from 'mdb-react-ui-kit';
 import Input from '../../Components/Input';
-import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faApple } from '@fortawesome/free-brands-svg-icons';
 
-const SignupPage = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    surname: '',
-    email: '',
-    password: '',
-  });
+const SignUp = () => {
+  const [userType, setUserType] = useState('individual');
+  const [rememberMe, setRememberMe] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [agreeContract, setAgreeContract] = useState(false);
+  const [receiveEmails, setReceiveEmails] = useState(false);
 
-  const [termsChecked, setTermsChecked] = useState(false); 
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const handleUserTypeChange = (type) => {
+    setUserType(type);
   };
 
-  const handleCheckboxChange = () => {
-    setTermsChecked(!termsChecked);
+  const handleAcceptTermsChange = () => {
+    setAgreeContract(!agreeContract);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onRememberMeChange = () => {
+    setRememberMe(!rememberMe);
+  };
 
-    if (!termsChecked) {
-      alert('Lütfen tüm hükümleri ve koşulları onaylayın.');
-      return;
-    }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    console.log('Signup form submitted:', formData);
+  const handleAgreeContractChange = () => {
+    setAgreeContract(!agreeContract);
+  };
 
+  const handleReceiveEmailsChange = () => {
+    setReceiveEmails(!receiveEmails);
   };
 
   return (
-    <div>
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50 card p-4 shadow">
+      <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleUserTypeChange('individual')} active={userType === 'individual'}>
+            <h5 className="font-weight-bold mb-0">Kişi</h5>
+          </MDBTabsLink>
 
-      <div className='d-flex justify-content-center align-items-center'>
-        <div className="w-75 h-100 d-flex flex-column align-items-center">
-          <h1 className="text-center">Kaydol</h1>
-          <div style={{ border: '2px solid #ccc', padding: '40px', borderRadius: '10px', margin: '100px auto', width: '500px' }}>
-            <form onSubmit={handleSubmit}>
-              <Input
-                label={"Ad"}
-                type={"text"}
-                name={"name"}
-                value={formData.name}
-                onChange={handleChange}
-              />
-              <Input
-                label={"Soyad"}
-                type={"text"}
-                name={"surname"}
-                value={formData.surname}
-                onChange={handleChange}
-              />
-              <Input
-                label={"Email"}
-                type={"email"}
-                name={"email"}
-                value={formData.email}
-                onChange={handleChange}
-              />
-              
-              <Input
-                label={"Şifre"}
-                type={"password"}
-                name={"password"}
-                value={formData.password}
-                onChange={handleChange}
-              />
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleUserTypeChange('company')} active={userType === 'company'}>
+            Şirket
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
 
-              <div className="form-group form-check">
-                <div className="d-flex align-items-center" style={{ marginBottom: '15px' }}>
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="termsCheckbox"
-                    checked={termsChecked}
-                    onChange={handleCheckboxChange}
-                  />
-                  <label className="form-check-label" htmlFor="termsCheckbox" style={{ marginLeft: '10px' }}>
-                    <span style={{ color: 'blue' }}>Tüm hükümleri ve koşulları </span>
-                    <span style={{ color: 'grey' }}>onaylıyorum.</span>
-                  </label>
-                </div>
-              </div>
-
-              <Button variant="primary" size="lg" type="submit" style={{ width: '100%' }}>
-                Hesap Oluştur
-              </Button>{' '}
-            </form>
+      {userType === 'individual' && (
+        <div>
+          <h5 className="mb-4">Kayıt Ol</h5>
+          <div className="row mb-4" >
+            <div className="col">
+              <Input label={"Ad"} type={"text"} name={"FirstName"} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div className="col">
+              <Input label={"Soyad"} type={"text"} name={"LastName"} value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
           </div>
-          <FontAwesomeIcon icon="fa-solid fa-right-to-bracket" />
-          <FontAwesomeIcon icon="fa-solid fa-piggy-bank" />
+          <Input label={"E-posta"} type={"text"} name={"Email"} />
+          <Input label={"Şifre"} type={showPassword ? "text" : "password"} name={"Password"} />
+          <div className="form-check mb-3">
+            <input className="form-check-input" type="checkbox" id="showPasswordCheckbox" onChange={togglePasswordVisibility} />
+            <label className="form-check-label" htmlFor="showPasswordCheckbox">
+              Şifreyi Göster
+            </label>
+          </div>
+          <div className="form-check mb-3">
+            <input className="form-check-input" type="checkbox" id="agreeContractCheckbox" onChange={handleAgreeContractChange} />
+            <label className="form-check-label" htmlFor="agreeContractCheckbox">
+              <span>
+                <Link to="/contract" style={{ color: 'blue', textDecoration: 'underline' }}>Hizmet sözleşmesini</Link>
+                <span className="ms-1" style={{ color: 'black' }}>onaylıyorum.</span>
+              </span>
+            </label>
+
+          </div>
+          <div className="form-check mb-3">
+            <input className="form-check-input" type="checkbox" id="receiveEmailsCheckbox" onChange={handleReceiveEmailsChange} />
+            <label className="form-check-label" htmlFor="receiveEmailsCheckbox">
+              İletişim bilgilerime e-ileti gönderilmesine izin veriyorum.
+            </label>
+          </div>
+          <div className="d-grid gap-2 col-8 mx-auto">
+            <button className="btn btn-primary" type="button">ÜYE OL</button>
+          </div>
+          <div className="mt-3 d-flex align-items-center">
+            <hr className="flex-grow-1" />
+            <span className="mx-2">veya</span>
+            <hr className="flex-grow-1" />
+          </div>
         </div>
+      )}
+
+      {userType === 'company' && (
+        <div>
+          <div className="row mb-4">
+            <Input label={"Firma Adı"} type={"text"} name={"CompanyName"} />
+            <div className="col">
+              <Input label={"Adınız"} type={"text"} name={"FirstName"} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div className="col">
+              <Input label={"Soyadınız"} type={"text"} name={"LastName"} value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
+          </div>
+          <Input label={"E posta adresiniz"} type={"text"} name={"Email"} />
+          <div className="row mb-4">
+            <div className="col">
+              <Input label={"Cep Telefonu"} type={"text"} name={"CellPhone"} />
+            </div>
+            <div className="col">
+              <Input label={"Telefon Numarası"} type={"text"} name={"PhoneNumber"} />
+            </div>
+          </div>
+          <div className="row mb-4">
+            <div className="col">
+              <Input label={"Şifre"} type={"password"} name={"Password"} />
+            </div>
+            <div className="col">
+              <Input label={"Şifreniz (Tekrar)"} type={"password"} name={"PasswordConfirm"} />
+            </div>
+            <label className="form-check-label text-muted">
+              -Rakam ve harf kullanınız
+            </label>
+          </div>
+          <div className="form-check mb-3">
+            <input className="form-check-input" type="checkbox" id="acceptTermsCheckbox" onChange={handleAcceptTermsChange} />
+            <label className="form-check-label" htmlFor="acceptTermsCheckbox">
+              <span>
+                <Link to="/contract" style={{ color: 'blue', textDecoration: 'underline' }}>Gizlilik Prensipleri ve Üyelik Koşulları ile Site Kullanım Şartları</Link>
+                <span className="ms-1" style={{ color: 'black' }}>kabul ediyorum ve Kişisel Verilerin Korunması Bilgilendirmesi'ni okudum, anladım..</span>
+              </span>
+            </label>
+
+          </div>
+
+          <div className="d-grid gap-2 col-8 mx-auto">
+            <button className="btn btn-primary" type="button">ÜYE OL</button>
+          </div>
+        </div>
+      )}
+
+      <div className="mt-3 d-flex">
+        <button className="btn btn-light border me-2 flex-grow-1" type="button">
+          <FontAwesomeIcon icon={faGoogle} className="me-2" />
+          Google ile Giriş Yap
+        </button>
+        <button className="btn btn-dark flex-grow-1" type="button">
+          <FontAwesomeIcon icon={faApple} className="me-2" />
+          Apple ile Giriş Yap
+        </button>
       </div>
-    </div>
+
+      <div className="text-center mt-3">
+        Zaten üye misin? <Link to="/login" style={{ color: 'black' }}>Giriş yap</Link>
+      </div>
+
+    </MDBContainer>
   );
 };
 
-export default SignupPage;
+export default SignUp;
