9b421eff90d7c2b136c0bebc19fddf0539baeaec
Anldmrlpp <147911313+Anldmrlpp@users.noreply.github.com>
2024-03-10 04:29:40 +0300

    contact

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Footer from '../../Components/Footer';
-import './Contact.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import './Contact.css'; // Import CSS file here
+import BannerImage from '../../images/BannerImage.jpg';
 
 const ContactPage = () => {
     return (
-        <section className="ftco-section img bg-hero" style={{backgroundImage: 'url(images/bg_1.jpg)'}}>
+        <section className="ftco-section img bg-hero" style={{backgroundImage: `url(${BannerImage})`}}>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6 text-center mb-5">
-                        
+                        <h2>Contact Us</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     </div>
                 </div>
                 <div className="row justify-content-center">
@@ -19,37 +19,40 @@ const ContactPage = () => {
                             <div className="row no-gutters justify-content-between">
                                 <div className="col-lg-6 d-flex align-items-stretch">
                                     <div className="info-wrap w-100 p-5">
-                                        <h3 className="mb-4">Contact us</h3>
                                         <div className="dbox w-100 d-flex align-items-start">
                                             <div className="icon d-flex align-items-center justify-content-center">
-                                                <span className="fa fa-map-marker"></span>
+                                                <i className="fas fa-home"></i>
                                             </div>
                                             <div className="text pl-4">
-                                                <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+                                                <h4>Address</h4>
+                                                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
                                             </div>
                                         </div>
                                         <div className="dbox w-100 d-flex align-items-start">
                                             <div className="icon d-flex align-items-center justify-content-center">
-                                                <span className="fa fa-phone"></span>
+                                                <i className="fas fa-phone"></i>
                                             </div>
                                             <div className="text pl-4">
-                                                <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 235sds5 98</a></p>
+                                                <h4>Phone</h4>
+                                                <p><a href="tel://1234567920">+ 1235 235sds5 98</a></p>
                                             </div>
                                         </div>
                                         <div className="dbox w-100 d-flex align-items-start">
                                             <div className="icon d-flex align-items-center justify-content-center">
-                                                <span className="fa fa-paper-plane"></span>
+                                                <i className="fas fa-envelope"></i>
                                             </div>
                                             <div className="text pl-4">
-                                                <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@d.com</a></p>
+                                                <h4>Email</h4>
+                                                <p><a href="mailto:info@yoursite.com">info@d.com</a></p>
                                             </div>
                                         </div>
                                         <div className="dbox w-100 d-flex align-items-start">
                                             <div className="icon d-flex align-items-center justify-content-center">
-                                                <span className="fa fa-globe"></span>
+                                                <i className="fas fa-globe"></i>
                                             </div>
                                             <div className="text pl-4">
-                                                <p><span>Website</span> <a href="#">yoursite.com</a></p>
+                                                <h4>Website</h4>
+                                                <p><a href="#">yoursite.com</a></p>
                                             </div>
                                         </div>
                                     </div>
@@ -57,10 +60,6 @@ const ContactPage = () => {
                                 <div className="col-lg-5">
                                     <div className="contact-wrap w-100 p-md-5 p-4">
                                         <h3 className="mb-4">Get in touch</h3>
-                                        <div id="form-message-warning" className="mb-4"></div>
-                                        <div id="form-message-success" className="mb-4">
-                                            Your message was sent, thank you!
-                                        </div>
                                         <form method="POST" id="contactForm" name="contactForm" noValidate="noValidate">
                                             <div className="row">
                                                 <div className="col-md-12">
@@ -99,7 +98,9 @@ const ContactPage = () => {
                 </div>
             </div>
 
-            <Footer />
+            <div className="mt-8"> {/* Here is the additional div for spacing */}
+                <Footer />
+            </div>
         </section>
     );
 };
