18345ce320ba3e3e1335e4d37fff7049aa754822
Anldmrlpp <147911313+Anldmrlpp@users.noreply.github.com>
2024-03-21 00:52:30 +0300

    contact

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -2,71 +2,82 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarker, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookSquare, faInstagram, faWhatsapp, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
-
 import './Contact.css';
 import Footer from '../../Components/Footer';
 
 const ContactPage = () => {
     return (
         <div id='contact'>
-            <div className="container">
+            <div className="container" >
                 <header>
-                    <h1>Contact Us</h1>
+                    <h1>BİZE ULAŞIN</h1>
                 </header>
                 <div className="content">
-                    <div className="content-form">
-                        <section>
-                            <FontAwesomeIcon icon={faMapMarker} size="2x" />
-                            <h2>Address</h2>
-                            <p>
-                                Lorem ipsum dolor sit. <br />
-                                Lorem, ipsum dolor. <br />
-                                lorem
-                            </p>
+                    <div className="content-form" >
+                        <section className="contact-info">
+                            <div className="icon-background">
+                                <FontAwesomeIcon icon={faMapMarker} size="2x" />
+                            </div>
+                            <div>
+                                <h2>Adres</h2>
+                                <p>
+                                    Lorem ipsum dolor sit. <br />
+                                    Lorem, ipsum dolor. <br />
+                                    lorem
+                                </p>
+                            </div>
                         </section>
-                        <section>
-                            <FontAwesomeIcon icon={faPhone} size="2x" />
-                            <h2>Phone</h2>
-                            <p>123-456-78901548</p>
+                        <section className="contact-info">
+                            <div className="icon-background">
+                                <FontAwesomeIcon icon={faPhone} size="2x" />
+                            </div>
+                            <div>
+                                <h2>Telefon</h2>
+                                <p>123-456-78901548</p>
+                            </div>
                         </section>
-                        <section>
-                            <FontAwesomeIcon icon={faEnvelope} size="2x" />
-                            <h2>E-mail</h2>
-                            <p>dghdgqd@temporary.net</p>
+                        <section className="contact-info">
+                            <div className="icon-background">
+                                <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                            </div>
+                            <div>
+                                <h2>E-mail</h2>
+                                <p>istegelsin@temporary.net</p>
+                            </div>
                         </section>
+
                     </div>
                     <form>
                         <div className="form">
                             <div className="contact-form">
-                                <div>
+                                <div className="input-wrapper">
                                     <input type="text" placeholder="Adınız" required />
-                                </div>
-                                <div>
                                     <input type="email" placeholder="E-mail" required />
                                 </div>
-                                <div>
+                                <div className="message-wrapper">
                                     <textarea placeholder="Mesajınızı Yazınız...." required></textarea>
                                 </div>
-                                <div>
+                                <div className="button-wrapper">
                                     <input type="submit" name="submit" />
                                 </div>
                             </div>
                         </div>
                     </form>
+                    </div>
+
+                        <div className="media">
+                            <ul className="list-inline ">
+                                <li><FontAwesomeIcon icon={faFacebookSquare} size="2x" /></li>
+                                <li><FontAwesomeIcon icon={faInstagram} size="2x" /></li>
+                                <li><FontAwesomeIcon icon={faWhatsapp} size="2x" /></li>
+                                <li><FontAwesomeIcon icon={faTwitterSquare} size="2x" /></li>
+                            </ul>
+                        </div>
+                        <div className="empty"></div>
                 </div>
-                <div className="media">
-                    <ul className="list-inline">
-                        <li><FontAwesomeIcon icon={faFacebookSquare} size="2x" /></li>
-                        <li><FontAwesomeIcon icon={faInstagram} size="2x" /></li>
-                        <li><FontAwesomeIcon icon={faWhatsapp} size="2x" /></li>
-                        <li><FontAwesomeIcon icon={faTwitterSquare} size="2x" /></li>
-                    </ul>
-                </div>
-                <div className="empty"></div>
-            </div>
                 <Footer />
-        </div>
-    );
+            </div>
+            );
 }
 
-export default ContactPage;
+            export default ContactPage;
