d3807e9203dfc10edee42f4ca184bc52d8f9f4b4
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2024-04-18 21:54:45 +0300

    Company için ilan oluşturma özelliği getirildi

--- src/views/AdvertisementProcedures/CreatedJobPosting.js
+++ src/views/AdvertisementProcedures/CreatedJobPosting.js
@@ -4,26 +4,29 @@ import Input from '../../Components/Input';
 import { useSelector } from 'react-redux';
 import { faSave } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { insertJobPosting } from '../../api/apiCalls';
 
-const CreatedJobPosting = () => {
+const CreatedJobPosting = (props) => {
 
     const [createdTitle, setCreatedTitle] = useState();
     const [createdDescription, setCreatedDescription] = useState();
     const [createdTotalSalary, setCreatedTotalSalary] = useState();
     const [createdWorkPerHour, setCreatedWorkPerHour] = useState();
-    const [createdCity, setCreatedCity] = useState();
-    const [createdDistrict, setCreatedDistrict] = useState();
-    const [createdStreet, setCreatedStreet] = useState();
-    const [createdEducationLevelName, setCreatedEducationLevelName] = useState();
-    const [createdEmploymentName, setCreatedEmploymentName] = useState();
-    const [createdExperienceLevelName, setCreatedExperienceLevelName] = useState();
+    const [createdAdress, setCreatedAdress] = useState();
+    const [createdEducationLevel, setCreatedEducationLevel] = useState();
+    const [createdEmploymentType, setCreatedEmploymentType] = useState();
+    const [createdExperienceLevel, setCreatedExperienceLevel] = useState();
     const [createdExperienceYears, setCreatedExperienceYears] = useState();
+    const [createdWorkModel, setCreatedWorkModel] = useState();
+    const [createdStartAt, setCreatedStartAt] = useState();
+    const [createdEndAt, setCreatedEndAt] = useState();
 
     const [advert, setAdvert] = useState();
     const [error, setError] = useState({});
 
-    const { companyId, logoPath } = useSelector(store => ({
+    const { companyId, email, logoPath } = useSelector(store => ({
         companyId: store.id,
+        email: store.email,
         logoPath: store.logoPath
     }));
 
@@ -51,39 +54,67 @@ const CreatedJobPosting = () => {
             setCreatedWorkPerHour(value);
             errorCopy['workPerHour'] = undefined;
         }
-        if(name == "createCity") {
-            setCreatedCity(value);
-            errorCopy['city'] = undefined;
+        if(name == "createAdress") {
+            setCreatedAdress(value);
+            errorCopy['adress'] = undefined;
         }
-        if(name == "createDistrict") {
-            setCreatedDistrict(value);
-            errorCopy['district'] = undefined;
+        if(name == "createEducationLevel") {
+            setCreatedEducationLevel(value);
+            errorCopy['educationLevel'] = undefined;
         }
-        if(name == "createStreet") {
-            setCreatedStreet(value);
-            errorCopy['street'] = undefined;
+        if(name == "createWorkModel") {
+            setCreatedWorkModel(value);
+            errorCopy['workModel'] = undefined;
         }
-        if(name == "createEducationLevelName") {
-            setCreatedEducationLevelName(value);
-            errorCopy['educationLevelName'] = undefined;
+        if(name == "createEmploymentType") {
+            setCreatedEmploymentType(value);
+            errorCopy['employmentType'] = undefined;
         }
-        if(name == "createEmploymentName") {
-            setCreatedEmploymentName(value);
-            errorCopy['employmentName'] = undefined;
-        }
-        if(name == "createExperienceLevelName") {
-            setCreatedExperienceLevelName(value);
-            errorCopy['experienceLevelName'] = undefined;
+        if(name == "createExperienceLevel") {
+            setCreatedExperienceLevel(value);
+            errorCopy['experienceLevel'] = undefined;
         }
         if(name == "createExperienceYears") {
             setCreatedExperienceYears(value);
             errorCopy['experienceYears'] = undefined;
         }
+        if(name == "createStartAt") {
+            setCreatedStartAt(value);
+            errorCopy['startAt'] = undefined;
+        }
+        if(name == "createEndAt") {
+            setCreatedEndAt(value);
+            errorCopy['endAt'] = undefined;
+        }
 
     }
 
-    const onClickSave = () => {
+    const onClickSave = async (event) => {
+        event.preventDefault();
 
+        const body = {
+            companyid: companyId,
+            employment_type: createdEmploymentType,
+            education_level: createdEducationLevel,
+            experience_level: createdExperienceLevel,
+            experience_years: createdExperienceYears,
+            work_model: createdWorkModel,
+            work_per_hour: createdWorkPerHour,
+            total_salary: createdTotalSalary,
+            title: createdTitle,
+            description: createdDescription,
+            adress: createdAdress,
+            is_active: true,
+            start_at: createdStartAt,
+            end_at: createdEndAt
+        }
+
+        try {
+            await insertJobPosting(body);
+            props.history.push(`/procedures/${email}/ilan-paneli`);
+        } catch(error) {
+            
+        }
     }
 
     return (
@@ -97,27 +128,29 @@ const CreatedJobPosting = () => {
                     </img>
                 </div>
                 <div className="card-body ps-5 pe-5">
-                    <Input name="createTitle" label="Başlığı Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan Başlığı"/>
+                    <Input name="createTitle" label="Başlığı Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan Başlığı"/>
 
-                    <Input name="createDescription" label="Açıklamayı Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan Açıklaması"/>
+                    <Input name="createDescription" label="Açıklamayı Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan Açıklaması"/>
 
-                    <Input name="createTotalSalary" label="Maaşı Değiştir (₺)" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışanın Aylık Maaşı"/>
+                    <Input name="createTotalSalary" label="Maaşı Gir (₺)" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışanın Aylık Maaşı"/>
 
-                    <Input name="createWorkPerHour" label="Saatlik Ücreti Değiştir (₺)" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışanın Saatlik Ücreti"/>
+                    <Input name="createWorkPerHour" label="Saatlik Ücreti Gir (₺)" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışanın Saatlik Ücreti"/>
 
-                    <Input name="createCity" label="Şehri Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İş Adresi"/>
+                    <Input name="createAdress" label="Adresi Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İş Adresi"/>
 
-                    <Input name="createDistrict" label="İlçeyi Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İş Adresi"/>
+                    <Input name="createEducationLevel" label="Eğitim Seviyesini Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan İçin Gerekli Eğtim Seviyesi"/>
 
-                    <Input name="createStreet" label="Sokağı Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İş Adresi"/>
+                    <Input name="createWorkModel" label="Çalışma Modelini Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışma Modelini"/>
 
-                    <Input name="createEducationLevelName" label="Eğitim Seviyesini Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan İçin Gerekli Eğtim Seviyesi"/>
+                    <Input name="createEmploymentType" label="Çalışma Şeklini Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışma Şekli"/>
 
-                    <Input name="createEmploymentName" label="Çalışma Şeklini Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="Çalışma Şekli"/>
+                    <Input name="createExperienceLevel" label="Deneyim Seviyesini Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan İçin Gerekli Deneyim Seviyesi"/>
 
-                    <Input name="createExperienceLevelName" label="Deneyim Seviyesini Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan İçin Gerekli Deneyim Seviyesi"/>
+                    <Input name="createExperienceYears" label="Deneyim Yılını Gir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan İçin Gerekli Deneyim Yılı"/>
 
-                    <Input name="createExperienceYears" label="Deneyim Yılını Değiştir" type="text" onChangeVeriables={onChange} error={error.name} placeholder="İlan İçin Gerekli Deneyim Yılı"/>
+                    <Input name="createStartAt" label="İş Başlangıç Tarihini Gir" onChangeVeriables={onChange} error={error.birthday} type="date"/>
+
+                    <Input name="createEndAt" label="İş Bitiş Tarihini Gir" onChangeVeriables={onChange} error={error.birthday} type="date"/>
 
                     <div>
                                     
